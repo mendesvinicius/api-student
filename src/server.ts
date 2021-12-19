bd23b0f1332e293a2db995f4272614cf1785d8a4
Marcio Vinicius Mendes Barbosa <mvmb@intranet.pitang.com>
2021-12-19 11:34:04 -0300

    [database] - database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
+import { createConnection } from "typeorm";
 import {ApolloServer } from 'apollo-server';
 import { buildSchema } from "type-graphql";
 import {StudentResolver} from './resolvers/studentResolver';
 
 async function runServer()
-{
+{  
+    createConnection();
     const schema = await buildSchema({
         resolvers:[
             StudentResolver
