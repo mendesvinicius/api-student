cc9a2863fe3920d478257c0ee810ab7c12b59ac5
Marcio Vinicius Mendes Barbosa <mvmb@intranet.pitang.com>
2021-12-19 11:14:51 -0300

    [Beginning] - first steps, initial setup

---
+++ src/resolvers/studentResolver.ts
@@ -0,0 +1,11 @@
+import { Resolver, Query } from "type-graphql";
+
+@Resolver()
+export class StudentResolver
+{
+    @Query(() => String)
+    hello()
+    {
+        return "Hello Student";
+    }
+}
